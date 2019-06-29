6c1a0c057736584e783ad9e089e9dc6829147b03
asdvxgxasjab <asdvxgxasjab@protonmail.com>
2019-06-28 22:31:17 -0700

    Revert "miner: every 5m display Mhash/s"
    This reverts commit c18f0c68f6758bff5ab3d038b8a77b2e9cecdc22.

--- miner.go
+++ miner.go
@@ -61,16 +61,11 @@ func (m *Miner) run() {
 	m.processor.RegisterForNewTransactions(newTxChan)
 	defer m.processor.UnregisterForNewTransactions(newTxChan)
 
-	ticker30s := time.NewTicker(30 * time.Second)
-	defer ticker30s.Stop()
-
-	ticker5m := time.NewTicker(5 * time.Minute)
-	defer ticker5m.Stop()
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
 
 	var block *Block
 	var targetInt *big.Int
-	var numHashes int64
-	var now, lastTime time.Time
 	for {
 		select {
 		case tip := <-tipChangeChan:
@@ -89,10 +84,7 @@ func (m *Miner) run() {
 				// ledger state is broken
 				panic(err)
 			}
-
 			targetInt = block.Header.Target.GetBigInt()
-			lastTime = time.Now()
-			numHashes = 0
 
 		case newTx := <-newTxChan:
 			log.Printf("Miner %d received notice of new transaction %s\n", m.num, newTx.TransactionID)
@@ -121,14 +113,7 @@ func (m *Miner) run() {
 				return
 			}
 
-		case <-ticker5m.C:
-			now = time.Now()
-			log.Printf("Miner %d current hash rate is %.2f Mhash/s\n",
-				m.num, (float64(numHashes)/now.Sub(lastTime).Seconds())/1000000.0)
-			lastTime = now
-			numHashes = 0
-
-		case <-ticker30s.C:
+		case <-ticker.C:
 			if block != nil {
 				// update block time every so often
 				block.Header.Time = time.Now().Unix()
@@ -146,14 +131,10 @@ func (m *Miner) run() {
 				if err != nil {
 					panic(err)
 				}
-
 				targetInt = block.Header.Target.GetBigInt()
-				lastTime = time.Now()
-				numHashes = 0
 			}
 
 			// hash the block and check the proof-of-work
-			numHashes++
 			idInt := block.Header.IDFast()
 			if idInt.Cmp(targetInt) <= 0 {
 				// found a solution
