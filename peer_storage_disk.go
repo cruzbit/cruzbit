56f4f806816b7ce241c829865bfebc53af36850a
asdvxgxasjab <asdvxgxasjab@protonmail.com>
2019-07-09 18:19:17 -0700

    randomly insert new peers into the peer store

--- peer_storage_disk.go
+++ peer_storage_disk.go
@@ -9,6 +9,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"math"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -46,7 +47,7 @@ func (p *PeerStorageDisk) Store(addr string) (bool, error) {
 		return false, nil
 	}
 
-	info := peerInfo{FirstSeen: time.Now().Unix()}
+	info := peerInfo{FirstSeen: time.Now().Unix(), LastAttempt: rand.Int63n(1 << 30)}
 	batch := new(leveldb.Batch)
 	if err := info.writeToBatch(addr, batch); err != nil {
 		return false, err
@@ -352,7 +353,7 @@ type peerInfo struct {
 
 // Should we retry this connection?
 func (p peerInfo) shouldRetry() bool {
-	if p.LastAttempt == 0 {
+	if p.LastAttempt < 1<<30 {
 		// never been tried
 		return true
 	}
